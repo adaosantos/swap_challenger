58ae39570c0bd4fa883cc2928f1866dbc3ffae01
Adão Santos <djadao@gmail.com>
2022-03-28 13:21:42 -0300

    update github token

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,6 @@ config :swap, ecto_repos: [Swap.Repo]
 config :swap, :github,
   url: "https://api.github.com",
   username: "adaosantos",
-  access_token: "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"
+  access_token: "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"
 
 import_config "#{config_env()}.exs"
