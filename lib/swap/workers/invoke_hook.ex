6fe56057a30561ade6bfd63e4103fbb8cd570606
Adão Santos <djadao@gmail.com>
2022-03-28 13:22:28 -0300

    added call on hook job

--- lib/swap/workers/invoke_hook.ex
+++ lib/swap/workers/invoke_hook.ex
@@ -2,11 +2,25 @@ defmodule Swap.Workers.InvokeHook do
   require Logger;
 
   alias Swap.Action;
-  
+
   use Kiq.Worker, dead: true, queue: "hooks", retry: 5
 
   @impl Kiq.Worker
   def perform([user, repo_id, issues, contributors]) do
     Action.list_by_repo(repo_id)
+    |> Enum.map ( fn (act) -> call_hook(act, issues, contributors) end)
+  end
+
+  defp call_hook(act, issues, contributors) do
+    HTTPoison.post! act.url, build_body(issues, contributors), headers
+  end
+
+  defp build_body(issues, contributors) do
+    %{ issues: issues, contributors: contributors }
+    |> Jason.encode!
+  end
+
+  defp headers do
+    [{"Content-Type", "application/json"}]
   end
 end
